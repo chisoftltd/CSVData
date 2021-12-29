a46240751b973c741bc1fa6753572fb26020cb8e
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-29 12:13:34 +0000

    Using pandas

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
-import csv
+# import csv
+#
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     tempareture = []
+#     for row in data:
+#         if row[1] != "temp":
+#             tempareture.append(row[1])
+#     print(tempareture)
 
-with open("weather_data.csv") as data_file:
-    data = csv.reader(data_file)
-    tempareture = []
-    for row in data:
-        if row[1] != "temp":
-            tempareture.append(row[1])
-    print(tempareture)
+import pandas as pd
+
+data = pd.read_csv("weather_data.csv")
+print(type(data))
+print(type(data['temp']))
