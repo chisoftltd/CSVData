4f347eb95c504e9a016ce7d7cea5a28b61043f32
Benjamin.Chinwe <chisoftltd@gmail.com>
2021-12-29 12:01:26 +0000

    Using import csv

--- main.py
+++ main.py
@@ -2,5 +2,8 @@ import csv
 
 with open("weather_data.csv") as data_file:
     data = csv.reader(data_file)
+    tempareture = []
     for row in data:
-        print(row)
+        if row[1] != "temp":
+            tempareture.append(row[1])
+    print(tempareture)
